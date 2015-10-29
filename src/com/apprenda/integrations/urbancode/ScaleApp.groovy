b74180ad56d6e8bebf7e20ccd6c1f3313fe61b34
Chris Dutra <cdutra@apprenda.com>
2015-10-29 13:53:38 -0400

    testing GitLab-TC-Slack workflow

--- src/com/apprenda/integrations/urbancode/ScaleApp.groovy
+++ src/com/apprenda/integrations/urbancode/ScaleApp.groovy
@@ -13,12 +13,6 @@ import com.urbancode.air.AirPluginTool
 import static groovyx.net.http.ContentType.*
 final def apTool = new AirPluginTool(this.args[0], this.args[1])
 final def props = apTool.getStepProperties()
-def paths = [Auth:'/authentication/api/v1/sessions/developer',
-	NewVersion:'/developer/api/v1/versions/',
-	NewApp:'/developer/api/v1/apps/',
-	GetAliases:'/developer/api/v1/apps/',
-	GetVersions:'/developer/api/v1/versions/',
-	Demote:'/developer/api/v1/versions/']
 
 def newVersionRequired = false
 println "Starting Apprenda Authentication"
@@ -26,7 +20,7 @@ def client = new RESTClient(props.ApprendaURL)
 // handle authentication
 client.handler.failure = client.handler.success
 client.ignoreSSLIssues()
-def resp = client.post(path:paths.Auth, body:[username:props.ApprendaUser, password:props.ApprendaPassword, tenantAlias:props.TenantAlias], requestContentType: JSON)
+def resp = client.post(path:Constants.REST_API_PATHS.Auth, body:[username:props.ApprendaUser, password:props.ApprendaPassword, tenantAlias:props.TenantAlias], requestContentType: JSON)
 println "Authentication Response Code: " + resp.status
 println "Authentication Response Data: " + resp.getData()
 def token = resp.getData().apprendaSessionToken
